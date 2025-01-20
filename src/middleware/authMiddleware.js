3f44dbbcbbf9ce9f15f96648d24c9bf33348d86b
Josephglz <joseph220199@gmail.com>
2025-01-20 17:50:47 -0600

    feat: add authentication middleware to protect routes with JWT TOKEN

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken'
+
+const protect = (req, res, next) => {
+    const token = req.header('Authorization')?.replace('Bearer ', '')
+
+    if(!token) {
+        return res.status(401).json({ message: 'No se ha proporcionado un token válido.' })
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_TOKEN)
+        req.user = decoded
+        next()
+    } catch (error) {
+        return res.status(401).json({ message: 'Token no válido.' })
+    }
+}
+
+export default protect
