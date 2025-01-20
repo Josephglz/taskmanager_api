faa455b78b15ebef3867fa65be96f3e705f3a9ee
Josephglz <joseph220199@gmail.com>
2025-01-20 17:50:22 -0600

    feat: add task routes for task management API

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 
 import authRoutes from './routes/authRoutes.js'
+import taskRoutes from './routes/taskRoutes.js'
 
 dotenv.config()
 
@@ -11,6 +12,7 @@ app.use(express.json())
 app.use(cors())
 
 app.use('/api/auth', authRoutes)
+app.use('/api', taskRoutes)
 
 const PORT = process.env.APP_PORT || 5000
 
