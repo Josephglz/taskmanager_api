acf7616d28d36ddcb9685e84b180ea1f93c06ac7
Josephglz <joseph220199@gmail.com>
2025-01-21 15:49:23 -0600

    feat: configure CORS options for specific origins and methods in index.js temporal

--- src/index.js
+++ src/index.js
@@ -9,7 +9,17 @@ dotenv.config()
 
 const app = express()
 app.use(express.json())
-app.use(cors())
+
+const corsOptions = {
+    origin: [
+        'http://localhost:3000',
+        'http://localhost:4200',
+        'http://192.168.100.3:4200'
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}
+app.use(cors(corsOptions))
 
 app.use('/api/auth', authRoutes)
 app.use('/api', taskRoutes)
