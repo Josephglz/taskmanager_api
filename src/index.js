cdf22b12d2b48c686b1510eb00a56a78eeb7e914
Josephglz <joseph220199@gmail.com>
2025-01-21 20:43:14 -0600

    fix: update environment variable for server port configuration

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ app.use(cors(corsOptions))
 app.use('/api/auth', authRoutes)
 app.use('/api', taskRoutes)
 
-const PORT = process.env.APP_PORT || 5000
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
